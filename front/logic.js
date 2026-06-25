9a45792f4dd313103e1b30d4a6022b4cee1f4701
mleonardi <mleonardi@pioix.edu.ar>
2026-06-24 22:12:34 -0300

    UPDATE
    modificaciones en logic y en el html, revisar que en el mayor y menor deje vacio en datos la parte de id_jugador, eso porque hay que unirlo con el codigo de login, donde está ese valor

--- front/logic.js
+++ front/logic.js
@@ -1,7 +1,11 @@
+let uno;
+let dos;
+let puntos = 0;
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-async function recibir_imagen() {
+
+async function recibir_id() {
     let res = await fetch("http://localhost:4000/cantantes")
     let respuesta = await res.json()
 
@@ -33,19 +37,64 @@ async function recibir_imagen() {
             }
         }
     }
-    let uno = respuesta[objeto]
-    let dos = respuesta[objeto2]
+    uno = respuesta[objeto]
+    dos = respuesta[objeto2]
     mostrar(respuesta, objeto, objeto2)
 
 }
 
 function mostrar(respuesta, objeto, objeto2){
     if(respuesta[objeto].id_categoria === respuesta[objeto2].id_categoria){
-        document.getElementById("img_1").innerHTML= `<img src="${respuesta[objeto].imagen_url}" alt="${respuesta[objeto].nombre}">`
-        document.getElementById("img_2").innerHTML= `<img src="${respuesta[objeto2].imagen_url}" alt="${respuesta[objeto2].nombre}">`
+        document.getElementById("img_1").src = respuesta[objeto].imagen_url
+        document.getElementById("img_1").alt = respuesta[objeto].nombre
+        document.getElementById("img_2").src = respuesta[objeto2].imagen_url
+        document.getElementById("img_2").alt = respuesta[objeto2].nombre
+        document.getElementById("valor1").innerHTML=`<p>${respuesta[objeto].valor}</p>`
         }else{
-            recibir_imagen()
+            recibir_id()
         }
+}
+
+function mayor(uno,dos){
+    console.log(uno)
+    console.log(dos)
+    if(uno.valor > dos.valor){
+        puntos++
+    }else{
+        let datos = {
+            puntaje: puntos,
+            fecha: Date,
+            id_jugador:
+        }
+        guardarPuntaje(datos)
+    }
+}
+
+function menor(){
+    if(uno.valor < dos.valor){
+        puntos++
+    }else{
+        let datos = {
+            puntaje: puntos,
+            fecha: Date,
+            id_jugador:
+        }
+        guardarPuntaje(datos)
+    }
+}
+
+async function guardarPuntaje(datos) {
+    const response = await fetch('http://localhost:4000/artistasPost',{
+        method: "POST", //GET, POST, PUT o DELETE
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datos) //JSON.stringify convierte de objeto a JSON
+    })
+    console.log(response)
+    let result = await response.json()//Desarma el json y lo arma como un objeto
+    console.log(result)
+
 }
 
 
