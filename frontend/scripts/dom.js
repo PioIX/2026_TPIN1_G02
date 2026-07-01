0c423efd44fbf1927842f0ac6c38f2b3d4e02f45
jbrizuela <jbrizuela@pioix.edu.ar>
2026-07-01 09:52:03 -0300

    Cambio en function mostrarTab
    Simplificación

--- frontend/scripts/dom.js
+++ frontend/scripts/dom.js
@@ -21,15 +21,10 @@ function mostrarMensajeRegistro(texto, esError = true) {
 
 // Cambia entre los tabs de login y registro
 function mostrarTab(tab) {
-    document.getElementById('formLogin').classList.remove('activo');
-    document.getElementById('formRegistro').classList.remove('activo');
-    document.querySelectorAll('.tab').forEach(t => t.classList.remove('activo'));
+    let esLogin = tab === 'login';
 
-    if (tab === 'login') {
-        document.getElementById('formLogin').classList.add('activo');
-        document.querySelectorAll('.tab')[0].classList.add('activo');
-    } else {
-        document.getElementById('formRegistro').classList.add('activo');
-        document.querySelectorAll('.tab')[1].classList.add('activo');
-    }
+    document.getElementById('formLogin').classList.toggle('activo', esLogin);
+    document.getElementById('formRegistro').classList.toggle('activo', !esLogin);
+    document.querySelectorAll('.tab')[0].classList.toggle('activo', esLogin);
+    document.querySelectorAll('.tab')[1].classList.toggle('activo', !esLogin);
 }
